515e0afa6ad6c99c8e87759a437b29b2bb6d227f
igor_orlov <thoronion@gmail.com>
2020-06-28 19:11:24 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function xmasTree(pyramidHeight, symbol) {
+    let xmas = '';
+
+    // Use variables xmas, pyramidHeight, symbol
+
+    return xmas;
+}
+
+// Check your code
+console.log(masTree(5, '*'));
+
+module.exports = { xmasTree };
