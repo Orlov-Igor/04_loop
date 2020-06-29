cfb867a49a679bffb2caae7b65eb4c22047400ec
igor_orlov <thoronion@gmail.com>
2020-06-29 20:44:34 +0300

    Test passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // function xmasTree(pyramidHeight, symbol) {
 //     let xmas = '';
-//     var pyramidHeight = prompt('Введите высоту пирамиды');
+//     var pyramidHeight /* = prompt('Введите высоту пирамиды')*/;
     
 //     for (var i = 1; i <= pyramidHeight; i++) {
 //       for (var j = 1; j <= pyramidHeight; j++) {
@@ -9,7 +9,7 @@
 //         };
 //         xmas += symbol;
 //       };
-//       xmas += "\n";
+//       xmas += '\n';
 //     };
     
 //     return xmas;
@@ -24,26 +24,34 @@
 
 function xmasTree(pyramidHeight, symbol) {
     let xmas = '';
-    var pyramidHeight = prompt('Введите высоту пирамиды');
-    var pyramidWidth = pyramidHeight * 2 - 1;
-    var space = " ";
+    var pyramidHeight /*= prompt('Введите высоту пирамиды')*/;
+    var space = ' ';
 
     for (var i = 1; i <= pyramidHeight; i++) {
-      for (var j = 1; j <= pyramidWidth; j++) {
-        if (j >= pyramidHeight - i && j <= pyramidHeight + i) {
+      
+        for (var j = 1; j <= pyramidHeight; j++) {
+        
+            if (j > pyramidHeight - i) {
             xmas += symbol;
-        } else {
+            } else {
             xmas += space;
-        }
+            }
         };
-      xmas += "\n";
+        
+        for (var j = 2; j <= pyramidHeight; j++) {
+          
+            if (j > i) {
+            break;
+            };
+            xmas += symbol;
+            };
+                
+      xmas += '\n';
     };
-    
     return xmas;
 }
 
-
-console.log(xmasTree(5, '*'));
+console.log(xmasTree(5, '$'));
 
 module.exports = { xmasTree };
 
