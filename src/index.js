7ff90894b407e1d48c2d4274020ae3d4d3bdc5d4
igor_orlov <thoronion@gmail.com>
2020-06-29 19:27:36 +0300

    Task #2 is done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
+// function xmasTree(pyramidHeight, symbol) {
+//     let xmas = '';
+//     var pyramidHeight = prompt('Введите высоту пирамиды');
+    
+//     for (var i = 1; i <= pyramidHeight; i++) {
+//       for (var j = 1; j <= pyramidHeight; j++) {
+//         if (j > i) {
+//           break;
+//         };
+//         xmas += symbol;
+//       };
+//       xmas += "\n";
+//     };
+    
+//     return xmas;
+// }
+
+
+// console.log(xmasTree(5, '*'));
+
+// module.exports = { xmasTree };
+
+
+
 function xmasTree(pyramidHeight, symbol) {
     let xmas = '';
     var pyramidHeight = prompt('Введите высоту пирамиды');
-    
+    var pyramidWidth = pyramidHeight * 2 - 1;
+    var space = " ";
+
     for (var i = 1; i <= pyramidHeight; i++) {
-      for (var j = 1; j <= pyramidHeight; j++) {
-        if (j > i) {
-          break;
+      for (var j = 1; j <= pyramidWidth; j++) {
+        if (j >= pyramidHeight - i && j <= pyramidHeight + i) {
+            xmas += symbol;
+        } else {
+            xmas += space;
+        }
         };
-        xmas += symbol;
-      };
       xmas += "\n";
     };
     
