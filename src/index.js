01036a2a520b8b8243de56722b1209fe4c858a41
igor_orlov <thoronion@gmail.com>
2020-07-02 20:03:35 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-// function xmasTree(pyramidHeight, symbol) {
-//     let xmas = '';
-//     var pyramidHeight /* = prompt('Введите высоту пирамиды')*/;
+// Домашка занятия №4
+
+function xmasTree(pyramidHeight, symbol) {
+    var xmas = '';
+    var pyramidHeight /* = prompt('Введите высоту пирамиды')*/;
     
-//     for (var i = 1; i <= pyramidHeight; i++) {
+    for (var i = 1; i <= pyramidHeight; i++) {
       
-//         for (var j = 1; j <= pyramidHeight; j++) {
+        for (var j = 1; j <= pyramidHeight; j++) {
         
-//           if (j > i) {
-//           break;
-//           };
-//           xmas += symbol;
-//         };
-//         xmas += '\n';
-//     };
+          if (j > i) {
+          break;
+          };
+          xmas += symbol;
+        };
+        xmas += '\n';
+    };
     
-//     return xmas;
-// }
+    return xmas;
+}
 
 
-// console.log(xmasTree(5, '*'));
+console.log(xmasTree(5, '*'));
 
-// module.exports = { xmasTree };
+module.exports = { xmasTree };
 
 
 
 function xmasTree(pyramidHeight, symbol) {
-    let xmas = '';
-    var pyramidHeight /*= prompt('Введите высоту пирамиды')*/;
+    var xmas = '';
+    var pyramidHeight = prompt('Введите высоту пирамиды');
     var space = ' ';
 
     for (var i = 1; i <= pyramidHeight; i++) {
@@ -56,5 +58,32 @@ function xmasTree(pyramidHeight, symbol) {
 console.log(xmasTree(5, '$'));
 
 module.exports = { xmasTree };
+
+
+
+// Домашка занятия №5
+
+function xmasTree(pyramidHeight, symbol) {
+    let xmas = '';
+    if (symbol.length > 1) {
+       alert('Введите один символ');
+    } else {
+      
+        for (let i = 1; i <= pyramidHeight; i++) {
+        xmas += symbol.repeat(i);
+        xmas += '\n';
+      };
+    }
+    
+    return xmas;
+};
+
+let vertSize = prompt('Введите высоту пирамиды');
+let symbol = prompt('Введите символ');
+
+console.log(xmasTree(vertSize, symbol));
+
+module.exports = { xmasTree };
+
 
 
